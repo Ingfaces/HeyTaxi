0dcf29068840a033a516fa97ca3722e31a88a057
Ingfaces <44330355+Ingfaces@users.noreply.github.com>
2018-11-16 17:49:26 -0500

    Update src/pages/home/home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegistroPage } from '../registro/registro';
+import { MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public menuCtrl: MenuController) {
+    
+  }
+  
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
 
+  cerrar(){
+		window.localStorage.removeItem("idUsuario");	
+		this.navCtrl.setRoot(LoginPage, {}, { animate: true, duration: 300 });
+	}
+
+  registar(){
+    window.localStorage.removeItem("idUsuario");  
+    this.menuCtrl.close();
+    this.navCtrl.push(RegistroPage, {}, { animate: true, duration: 300 });
   }
 
 }
